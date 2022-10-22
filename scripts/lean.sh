72e3e074d65c72bedc6e91a63dc872d2a5769518
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-10-22 09:55:20 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -150,3 +150,4 @@ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_genera
 # 删除定时coremark
 rm -rf ./customfeeds/packages/utils/coremark
 svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/packages/utils/coremark
+sed -i 's/5.4/15/g' target/linux/rockchip/Makefile
